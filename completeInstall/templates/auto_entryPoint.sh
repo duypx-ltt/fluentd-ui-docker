155d4f0f498c57d1aa1c52f5401a6c8644d402f0
Sahil Ahuja <sahilahuja@gmail.com>
2016-06-28 12:12:21 +0530

    Last attempt on fluentd before moving to td-agent

--- completeInstall/templates/auto_entryPoint.sh
+++ completeInstall/templates/auto_entryPoint.sh
@@ -2,8 +2,10 @@
 set -x
 echo "Image ENTRYPOINT executing in `pwd` with arguments $*"
 
-source /usr/local/rvm/scripts/rvm
-#rvm use 2.3
-
 #Run command passed as argument (Else CMD gets executed, and persisted on commit)
-sh -c "$*"
+#Starting after bash login so rvm gets initialized
+
+bash -l -c "ruby /usr/local/rvm/gems/ruby-2.3.0/bin/fluentd -c /root/.fluentd-ui/fluent.conf -d /root/.fluentd-ui/fluent.pid -o /root/.fluentd-ui/fluent.log"
+
+cd /opt/build/fluentd-ui #fluend-ui command works only if run from this folder
+bash -l -c "$*"
