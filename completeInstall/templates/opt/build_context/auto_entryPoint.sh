dc7aa60c34cd0d4f0c51bb98953a8cda60b9c1a8
Sahil Ahuja <sahilahuja@gmail.com>
2016-06-28 12:25:54 +0530

    Correcting entryPoint command

--- completeInstall/templates/opt/build_context/auto_entryPoint.sh
+++ completeInstall/templates/opt/build_context/auto_entryPoint.sh
@@ -5,7 +5,7 @@ echo "Image ENTRYPOINT executing in `pwd` with arguments $*"
 #Run command passed as argument (Else CMD gets executed, and persisted on commit)
 #Starting after bash login so rvm gets initialized
 
-bash -l -c "ruby /usr/local/rvm/gems/ruby-2.3.0/bin/fluentd -c /root/.fluentd-ui/fluent.conf -d /root/.fluentd-ui/fluent.pid -o /root/.fluentd-ui/fluent.log"
+/opt/td-agent/embedded/bin/ruby /usr/sbin/td-agent --log /var/log/td-agent/td-agent.log --daemon /var/run/td-agent/td-agent.pid
 
 cd /opt/build/fluentd-ui #fluend-ui command works only if run from this folder
 bash -l -c "$*"
