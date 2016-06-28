5764557a43805d3bbcbb2a4670e9c4d67111cdb7
Sahil Ahuja <sahilahuja@gmail.com>
2016-06-28 05:46:26 +0530

    Stuff

--- completeInstall/templates/auto_entryPoint.sh
+++ completeInstall/templates/auto_entryPoint.sh
@@ -3,7 +3,7 @@ set -x
 echo "Image ENTRYPOINT executing in `pwd` with arguments $*"
 
 source /usr/local/rvm/scripts/rvm
-rvm use 2.3
+#rvm use 2.3
 
 #Run command passed as argument (Else CMD gets executed, and persisted on commit)
 sh -c "$*"
